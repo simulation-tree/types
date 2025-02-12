ee4d042728112c6bb3fa0385080c0dc4ed22613a
Phill <philldamaskin@gmail.com>
2025-02-11 21:05:15 -0500

    test against another type in custom type bank

--- tests/BankTests.cs
+++ tests/BankTests.cs
@@ -8,16 +8,16 @@ namespace Types.Tests
         [Test]
         public void LoadCustomBank()
         {
-            Assert.That(TypeRegistry.IsRegistered<DateTime>(), Is.False);
+            Assert.That(TypeRegistry.IsRegistered<TypeLayout.Variable>(), Is.False);
             TypeRegistry.Load<CustomTypeBank>();
-            Assert.That(TypeRegistry.IsRegistered<DateTime>(), Is.True);
+            Assert.That(TypeRegistry.IsRegistered<TypeLayout.Variable>(), Is.True);
         }
 
         public readonly struct CustomTypeBank : ITypeBank
         {
             void ITypeBank.Load(Register register)
             {
-                register.Invoke<DateTime>();
+                register.Invoke<TypeLayout.Variable>();
             }
         }
     }
